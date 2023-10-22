55cfcffc0362c7cd3baff2cd6885e0d621e6a25c
Grig Alex <alexengrigdev@gmail.com>
2023-10-22 03:25:00 +0300

    Add filters

---
+++ src/main/java/io/github/projectdepbro/gradle/plugin/filter/DependencyFilter.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 The Project DepBro Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.projectdepbro.gradle.plugin.filter;
+
+import org.gradle.api.artifacts.Dependency;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public interface DependencyFilter extends Predicate<Dependency> {
+
+    static DependencyFilter ofGroupRegex(String groupRegex) {
+        return dependency -> dependency.getGroup() != null && dependency.getGroup().matches(groupRegex);
+    }
+
+    @Override
+    boolean test(Dependency dependency);
+
+    @Override
+    default DependencyFilter negate() {
+        return dependency -> !test(dependency);
+    }
+
+    default DependencyFilter and(DependencyFilter other) {
+        Objects.requireNonNull(other);
+        return dependency -> test(dependency) && other.test(dependency);
+    }
+
+}
